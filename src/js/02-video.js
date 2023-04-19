70c5e460c6c8aaabef8c21f1b9cbf641f762dace
Enab13d <extendeddirection@gmail.com>
2023-04-19 21:55:20 +0300

    v 1.3
    - added script providing transfer input  data from form to local storage and autofill input fields with such data

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,16 +3,14 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = throttle(function(data) {
+const onPlay = throttle(function() {
 player.getCurrentTime().then(function(seconds) {
     let currentTime = seconds;
-    console.log(currentTime);
     localStorage.setItem("videoplayer-current-time", currentTime);
 }).catch(function(error) {
     console.log(error.name);
 });
-}, wait=1000);
+}, 1000);
 player.on('timeupdate', onPlay);
 const savedTime = Number(localStorage.getItem("videoplayer-current-time"));
 player.setCurrentTime(savedTime);
-console.log(throttle);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+const formEl = document.querySelector('.feedback-form');
+const submitBtn = document.querySelector('button[type=submit]');
+let obj;
+let parsedObj;
+const {
+  elements: { email, message },
+} = formEl;
+const onInput = () => {
+  obj = { email: email.value, message: message.value };
+  console.log(obj);
+  try {
+    stringifiedObj = JSON.stringify(obj);
+    console.log();
+  } catch (error) {
+    error.name;
+  }
+  localStorage.setItem('feedback-form-state', stringifiedObj);
+};
+onSubmit = e => {
+  e.preventDefault();
+  console.log(obj);
+  e.currentTarget.reset();
+};
+const checkLocalStorage = () => {
+  if (localStorage) {
+    try {
+      parsedObj = JSON.parse(localStorage.getItem('feedback-form-state'));
+      email.value = parsedObj.email;
+      message.value = parsedObj.message;
+      return;
+    } catch (error) {
+      error.name;
+    }
+  }
+};
 
+formEl.addEventListener('submit', onSubmit);
+formEl.addEventListener('input', throttle(onInput, 500));
+checkLocalStorage();
